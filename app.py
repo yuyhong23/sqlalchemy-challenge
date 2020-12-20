1332f85888feaa7289b5285614030b265803916d
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-12-19 17:37:04 -0800

    Created a file called app.py for Flask API.

---
+++ app.py
@@ -0,0 +1,42 @@
+import sqlalchemy
+#from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+from sqlalchemy.ext.automap import automap_base
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
